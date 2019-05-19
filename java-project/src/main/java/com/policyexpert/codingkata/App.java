11fced305910d4d439b5b6068193f1ad46ae46f2
prashanth shivakumar <prashanth.shivakumar@directlinegroup.co.uk>
2019-05-19 10:24:38 +0100

    Project template created

---
+++ java-project/src/main/java/com/policyexpert/codingkata/App.java
@@ -0,0 +1,13 @@
+package com.policyexpert.codingkata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
